ae7ccf0a5fe7bdc36a8fd6bd23d9fb7a6ae1502e
Averyrj <averyj54321@gmail.com>
2022-05-19 16:56:51 +1000

    core ui

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ let computerScore = 0;
 const rockbtn = document.getElementById('rock');
 const paperbtn = document.getElementById('paper');
 const scissorsbtn = document.getElementById('scissors');
+const resetbtn = document.getElementById('reset');
+const playerScore_span = document.getElementById('playerScore');
+const computerScore_span = document.getElementById('computerScore');
+
 
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3)
@@ -18,25 +22,46 @@ function computerPlay() {
             return s
     }
 }
-const computerSelection = computerPlay();
-const playerSelection = 'Rock'
-console.log(computerSelection)
-console.log(playerSelection)
-function playRound(playerSelection, computerSelection) {
+rockbtn.addEventListener('click', () => {
+    playRound(r);
+});
+paperbtn.addEventListener('click', () => {
+    playRound(p);
+});
+scissorsbtn.addEventListener('click', () => {
+    playRound(s);
+});
+resetbtn.addEventListener('click', () => {
+    reset();
+});
+function playRound(playerSelection) {
+    const computerSelection = computerPlay();
+    console.log(computerSelection);
+    console.log(playerSelection);
     if (playerSelection === 'Rock' && computerSelection === 'Scissors' || playerSelection === p && computerSelection === r || playerSelection === s && computerSelection === p) {
-        playerScore++
+        playerScore++;
+        updateScore();
         return "Player wins"
 
     } else if (playerSelection === 'Rock' && computerSelection === 'Paper' || playerSelection === p && computerSelection === s || playerSelection === s && computerSelection === r) {
         computerScore++
+        updateScore();
         return "Player loses"
     }
     else (playerSelection === computerSelection); {
         return "Tie"
     };
 }
-const roundResult = playRound(playerSelection, computerSelection)
-console.log(roundResult)
+function updateScore() {
+    playerScore_span.innerText = playerScore;
+    computerScore_span.innerText = computerScore;
+};
+function reset() {
+    playerScore = 0;
+    computerScore = 0;
+    updateScore();
+}
+
 console.log(playerScore)
 function game() {
     if (playerScore === 5) {
@@ -46,6 +71,3 @@ function game() {
         return "Computer wins, end of game"
     }
 }
-rockbtn.addEventListener('click', () => {
-    alert("Hello World");
-});
